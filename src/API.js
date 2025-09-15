bdb8a7e94ff5df435cb211750aa1a9cda054c09e
vinayKumar1332 <vinaypudi1332@gmail.com>
2025-09-15 23:23:18 +0530

    quick fix 2

---
+++ src/API.js
@@ -0,0 +1,11 @@
+
+
+// Orders API
+const VITE_ORDERS_API =
+  "https://script.google.com/macros/s/AKfycbx3duJLyCpMpvdOrEk4_ixxb4ooXM9rB8rPmP-Uk8hqvx00XAfjL135J8mZZWIy2jz5/exec";
+
+// Products API
+const VITE_PRODUCTS_API =
+  "https://script.google.com/macros/s/AKfycbyHsJIdbUWjsqEdxgS710XauKdcc8FyNOU4dkMXVt3UEObJ8V-y_kAj7GcQOoCCsFbZ/exec";
+
+export { VITE_ORDERS_API, VITE_PRODUCTS_API };
