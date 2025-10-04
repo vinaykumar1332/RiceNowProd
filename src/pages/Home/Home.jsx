668d76edaed8961a5bc2b4cb0e796e842806a837
vinay kumar <vinaypudi1332@gmail.com>
2025-10-04 22:34:55 +0530

    cards session storage fix-1

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import "./Home.css";
-import Hero from "./HeroBanner/hero"
+
 
 export default function Home() {
   return (
     <>
-  <Hero />
     <div className="home-page-container">
       <div style={{ maxWidth: 1200, margin: "0 auto", padding: "2rem" }}>
         <h1>Welcome to Ricenow</h1>
